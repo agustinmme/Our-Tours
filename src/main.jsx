582f2f26ceadb4b92a8ca475e62becc5cb017bb7
agustinmme <76716674+agustinmme@users.noreply.github.com>
2021-08-04 14:40:41 -0300

    Fix:bg

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { ChakraProvider,Box } from "@chakra-ui/react"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ChakraProvider, Box } from "@chakra-ui/react";
 ReactDOM.render(
-<ChakraProvider>
-<Box bg="gray.700" >
-          <App />
-      </Box>
-    </ChakraProvider>,
-  document.getElementById('root')
-)
+  <Box bg="gray.700">
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </Box>,
+  document.getElementById("root")
+);
